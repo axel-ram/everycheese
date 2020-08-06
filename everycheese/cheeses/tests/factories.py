65b96e9216b19d5c581e370f3001ab740f34b00f
destro\ram <ramsharma.axel@gmail.com>
2020-08-06 22:49:08 +0530

    added county of origin

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -5,12 +5,15 @@ import factory.fuzzy
 from ..models import Cheese
 
 class CheeseFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = Cheese
+
 	name = factory.fuzzy.FuzzyText()
 	slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
 	description = factory.Faker('paragraph', nb_sentences = 3, variable_nb_sentences=True, ext_word_list=None)
 	firmness = factory.fuzzy.FuzzyChoice(
 		[x[0] for x in Cheese.Firmness.choices]
 	)
+	country_of_origin = factory.Faker('country_code')
 
-	class Meta:
-		model = Cheese
+	
